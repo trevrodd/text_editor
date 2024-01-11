514d39b001de018b6005e9989c4838c55a987d2a
trevrodd <trodda07@uab.edu>
2024-01-11 16:16:47 -0600

    Add buttons and fix frame so buttons start from top of the column

--- tkinter_editor.py
+++ tkinter_editor.py
@@ -9,6 +9,13 @@ def main():
     text_edit = tk.Text(window, font="Helvetica 16")
     text_edit.grid(row=0, column=1)
 
+    frame = tk.Frame(window, relief=tk.RAISED, bd=2)
+    save_button = tk.Button(frame, text="Save")
+    open_button = tk.Button(frame, text="Open")
+
+    save_button.grid(row=0, column=0)
+    open_button.grid(row=1, column=0)
+    frame.grid(row=0, column=0, sticky="ns")
 
     window.mainloop()
 
