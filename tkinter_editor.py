e07724df0dff25b14c23791280e4a8fce792618a
trevrodd <trodda07@uab.edu>
2024-01-11 14:36:16 -0600

    Setting up initial window. Configure column/row size and font.

--- tkinter_editor.py
+++ tkinter_editor.py
@@ -1,4 +1,20 @@
 import tkinter as tk
 
-def open_file():
-    pass
+def main():
+    window = tk.Tk()
+    window.title("Text Editor")
+    window.rowconfigure(0, minsize=400)
+    window.columnconfigure(1, minsize=500)
+
+    text_edit = tk.Text(window, font="Helvetica 16")
+    text_edit.grid(row=0, column=1)
+
+
+    window.mainloop()
+
+
+
+
+
+
+main()
